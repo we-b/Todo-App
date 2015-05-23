af4f09a84090e5bf8325abcfecbd97aaadff793f
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-05-23 22:51:14 +0900

    Implement to show table view cell

--- Todo-App/Todo-App/TodoListTableViewController.swift
+++ Todo-App/Todo-App/TodoListTableViewController.swift
@@ -30,24 +30,24 @@ class TodoListTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return 7
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
 
-        // Configure the cell...
-
+        cell.textLabel?.text = "セクション\(indexPath.section)の\(indexPath.row)行目"
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
