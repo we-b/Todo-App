8e1f728544cdb754b332fc2508c1409ad5f1c757
shinbo maiki <s.c.c.no.yk.future.0603@gmail.com>
2015-05-28 00:58:38 +0900

    Create TodoCollection

---
+++ Todo-App/Todo-App/TodoCollection.swift
@@ -0,0 +1,13 @@
+//
+//  TodoCollection.swift
+//  Todo-App
+//
+//  Created by shinbo maiki on 2015/05/27.
+//  Copyright (c) 2015年 shinbo maiki. All rights reserved.
+//
+
+import UIKit
+
+class TodoCollection: NSObject {
+    var todos:[Todo] = []
+}
